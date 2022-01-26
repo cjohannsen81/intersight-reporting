d8ce9c5d6fd9ef7e386231cfd684ecad7b3ea2d6
Christian Johannsen <cjohannsen@Christians-MacBook-Air-2.local>
2022-01-26 11:28:52 -0800

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # intersight-reporting
-Python script to collect data from Cisco Intersight
+This python script is used to collect data from Cisco Intersight. The focus was to gather information about all servers that use SD-Cards and Controllers and check if there's another storage controller available.
+
+## Setup
+There are some requirements:
+1. Python on your machine
+Install Python3 on your machine: https://www.python.org/downloads/
+
+2. Intersight credentials
+The easiest way is to create API Keys in Intersight: https://intersight.com/apidocs/introduction/security/ and save them as environment variables using the `export` command.
+
+More information and the API explorer can be found here: https://intersight.com/apidocs/apirefs/aaa/AuditRecords/model/
+
+## Contribution
+Feel free to open an issue or pull request if you want to make this better!
---
+++ get_sd-cards.py
@@ -0,0 +1,74 @@
+import intersight.api.storage_api
+import intersight.api.compute_api
+import credentials
+import csv
+
+header = ["model","serial","rack/blade","name","vendor","model","pci_slot","flash_controller","flash_controller_state","vendor","pysical_card_type","physical_card_status","pysical_card_type","physical_card_status"]
+data = []
+
+def get_flex_storage(apiClient):
+    api_instance = intersight.api.storage_api.StorageApi(apiClient)
+    flash_controllers = api_instance.get_storage_flex_flash_controller_list()
+    for fcontroller in flash_controllers.results:
+        #uses the compute board moid to gather compute unit data
+        compute_board = fcontroller["compute_board"]["moid"]
+        get_compute(apiClient,compute_board)
+        data.append(fcontroller["model"])
+        data.append(fcontroller["controller_state"])
+        data.append(fcontroller["vendor"])
+        physical_drives = i["flex_flash_physical_drives"]
+        for p_drive in physical_drives:
+            physical_drive = p_drive["moid"]
+            get_sd_drives(apiClient,physical_drive)
+        print(data)
+        #writes the data array into the csv file
+        with open('sd_info.csv', 'a', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(data)
+        data.clear()
+
+def get_sd_drives(apiClient,physical_drive):
+    api_instance = intersight.api.storage_api.StorageApi(apiClient)
+    flash_drives = api_instance.get_storage_flex_flash_physical_drive_by_moid(physical_drive)
+    data.append(flash_drives["card_type"])
+    data.append(flash_drives["card_status"])
+
+def get_compute(apiClient,compute_unit):
+    api_instance = intersight.api.compute_api.ComputeApi(apiClient)
+    summary = api_instance.get_compute_board_by_moid(compute_unit)
+    data.append(summary["model"])
+    data.append(summary["serial"])
+    if summary["compute_rack_unit"]:
+        rack_unit = summary["compute_rack_unit"]["moid"]
+        mapping = api_instance.get_compute_physical_summary_by_moid(rack_unit)
+        data.append("Rack-Server")
+        data.append(mapping["name"])
+    elif summary["compute_blade"]:
+        blade_unit = summary["compute_blade"]["moid"]
+        mapping = api_instance.get_compute_physical_summary_by_moid(blade_unit)
+        data.append("Blade-Server")
+        data.append(mapping["name"])
+    storage_controllers = summary["storage_controllers"]
+    for i in storage_controllers:
+        moid = i["moid"]
+        get_storage_controller(apiClient,moid)
+
+def get_storage_controller(apiClient,moid):
+    api_instance = intersight.api.storage_api.StorageApi(apiClient)
+    storage_controller = api_instance.get_storage_controller_by_moid(moid)
+    data.append(storage_controller["vendor"])
+    data.append(storage_controller["model"])
+    data.append(storage_controller["pci_slot"])
+
+def main():
+    apiClient = credentials.config_credentials()
+    try:
+        with open('sd_info.csv', 'a', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+        get_flex_storage(apiClient)
+    except intersight.OpenApiException as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
